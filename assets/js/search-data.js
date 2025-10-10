0f3516692d75a1167c0f51b786ff0631a20ea2a6
smsnobin77 <smsarwar96@gmail.com>
2025-10-10 08:19:06 +0000

    Deploying to gh-pages from @ smsnobin77/smsnobin77.github.io@5bf6638e2e41e8a196a1c4efb2e04666ef51c04d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,20 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Materials for courses you taught. Replace this text with your description.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
