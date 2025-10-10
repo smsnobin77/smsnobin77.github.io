f3a3f751c5485dcb8ac060d53892eb0da165c3d7
smsnobin77 <smsarwar96@gmail.com>
2025-10-10 08:59:46 +0000

    Deploying to gh-pages from @ smsnobin77/smsnobin77.github.io@cf37e26f3a1db304b2ba46fd65d2aea5dee73bcf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,20 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
