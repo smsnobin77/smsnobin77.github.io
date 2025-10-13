5057af90f5e3abdeb0a85a809c5d09358adc937f
smsnobin77 <smsarwar96@gmail.com>
2025-10-13 04:11:45 +0000

    Deploying to gh-pages from @ smsnobin77/smsnobin77.github.io@7eb9d94fd08107915d1f684403905c964a6d0b86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-repositories",
+          title: "Repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
